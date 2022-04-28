2d022862b246435d0fb44cf6729bd9336a4d0695
Kareem <kalaa95@gmail.com>
2022-04-28 16:13:33 +0200

    First Push

--- common.py
+++ common.py
@@ -4,8 +4,8 @@ import logging
 # mode = "prod" for heroku "dev" for running locally
 
 mode = "prod" 
-TELEGRAM_ACCESS_TOKEN = ""
-HEROKU_APP_NAME = ""
+TELEGRAM_ACCESS_TOKEN = "5349903888:AAGhbsTC5dPW04nFX2x-9hk-nDgQzukL9Fk"
+HEROKU_APP_NAME = "libgenzerodawn"
 LIBGEN_DOMAIN = "https://libgen.is/"
 
 # Enable logging
